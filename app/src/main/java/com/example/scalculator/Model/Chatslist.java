df8743c674d5bd3fc992e32dbfaa7b72f8a32132
poornima835 <67419124+poornima835@users.noreply.github.com>
2021-10-03 18:57:09 +0530

    Commit commit

---
+++ app/src/main/java/com/example/scalculator/Model/Chatslist.java
@@ -0,0 +1,22 @@
+package com.example.scalculator.Model;
+
+public class Chatslist {
+
+    String id;
+
+    public Chatslist(String id) {
+        this.id = id;
+    }
+
+
+    public Chatslist() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/scalculator/StartActivity.java
@@ -0,0 +1,53 @@
+package com.example.scalculator;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button login, register;
+
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+        //cast the views
+
+        login = findViewById(R.id.loginBtn);
+        register = findViewById(R.id.registerbtn);
+
+
+        //redirecting to respective activities
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+            }
+        });
+
+
+    }
+
+}
